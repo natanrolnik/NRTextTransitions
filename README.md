3391260dba3f67da355cf6a9f4cd373f8262f556
Natan Rolnik <natanrolnik@gmail.com>
2014-02-26 17:14:32 +0200

    Updates readme file

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 NRTextTransitions
 =================
 
-Basic, block based, text transitions
+#Basic, block based, text transitions
+
+NRTextTransitions is a category in UIView that makes easier handling animated text transitions for UILabel, UITextView and UITextField. As the standard +[UIView animateWithDuration:animations:] is not able to animate non-animatable properties, and text, font and textColor properties of UILabel, UITextView and UITextField, are non-animatable, you need to use CATransition to do so with animation.
+ 
+NRTextTransitions uses a similar API to the standard UIView class methods, encapsulating the changes passed in the `animations` parameter block. In order to apply the transitions to the objects mentioned in this block, you **MUST** create an array, add the objects to this array, and pass it in the 'textObjects' parameter.
+
+#Example
--- UIView+NRTextTransitions.h
+++ UIView+NRTextTransitions.h
@@ -11,7 +11,7 @@
 /**
  NRTextTransitions is a category in UIView that makes easier handling animated text transitions for UILabel, UITextView and UITextField. As the standard +[UIView animateWithDuration:animations:] is not able to animate non-animatable properties, and text, font and textColor properties of UILabel, UITextView and UITextField, are non-animatable, you need to use CATransition to do so with animation.
  
- NRTextTransition uses a similar API to the standard UIView class methods, encapsulating the changes passed in the `animations` parameter block. In order to apply the transitions to the objects mentioned in this block, you **MUST** create an array, add the objects to this array, and pass it in the 'textObjects' parameter.
+ NRTextTransitions uses a similar API to the standard UIView class methods, encapsulating the changes passed in the `animations` parameter block. In order to apply the transitions to the objects mentioned in this block, you **MUST** create an array, add the objects to this array, and pass it in the 'textObjects' parameter.
  */
 @interface UIView (NRTextTransitions)
 
